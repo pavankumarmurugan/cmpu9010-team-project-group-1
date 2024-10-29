c230a655033126ae7fc73cc6e1d361329b7c03a4
Salil Luley <salil.luley@gmail.com>
2024-10-29 22:15:20 +0000

    Added get friends api & refactored faiss implementation, moved it to service.

--- smartwardrobe-backend/src/infrastructure/services/services.module.ts
+++ smartwardrobe-backend/src/infrastructure/services/services.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { SearchService } from './search/search';
 import { UploadProfilePictureService } from './uploadProfilePicture/upload-profile-picture';
+import { FaissService } from './faiss/faiss.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ImageClusterModel } from '../frameworks/data-services/model/image-clusters.model';
+import { DataServicesModule } from './data-services/data-service.module';
 
 @Module({
-  imports: [HttpModule],
-  providers: [SearchService, UploadProfilePictureService],
-  exports: [SearchService, UploadProfilePictureService],
+  imports: [
+    HttpModule,
+    DataServicesModule,
+    TypeOrmModule.forFeature([ImageClusterModel]),
+  ],
+  providers: [SearchService, UploadProfilePictureService, FaissService],
+  exports: [SearchService, UploadProfilePictureService, FaissService],
 })
 export class ServicesModule {}
--- smartwardrobe-backend/src/use-cases/recommendation/recommendation.usecase.ts
+++ smartwardrobe-backend/src/use-cases/recommendation/recommendation.usecase.ts
@@ -1,119 +1,184 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import * as faiss from 'faiss-node';
-import { ImageClusterModel } from 'src/infrastructure/frameworks/data-services/model/image-clusters.model';
-import { ImageClusterEntity } from 'src/core/entities/image-cluster/image-cluster.entity';
-import { MESSAGES } from 'src/infrastructure/common/enum.ts/messages';
+// import { Injectable, OnModuleInit } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+// import * as faiss from 'faiss-node';
+// import { ImageClusterModel } from 'src/infrastructure/frameworks/data-services/model/image-clusters.model';
+// import { ImageClusterEntity } from 'src/core/entities/image-cluster/image-cluster.entity';
+// import { MESSAGES } from 'src/infrastructure/common/enum.ts/messages';
+// import { IDataServices } from 'src/core/abstracts';
+// import { RecommendationsResDto } from 'src/core/dto/recommendations/recommendations-res-dto';
+// import { IResponse } from 'src/core/interface/response.interface';
+
+// @Injectable()
+// export class RecommendationUsecase implements OnModuleInit {
+//   private faissIndex: faiss.IndexFlatL2;
+//   private imageData: ImageClusterEntity[] = [];
+//   private embeddingMatrix: number[][] = [];
+
+//   constructor(
+//     @InjectRepository(ImageClusterModel)
+//     private readonly imageClusterRepository: Repository<ImageClusterEntity>,
+//     private dataService: IDataServices,
+//   ) {}
+
+//   async onModuleInit() {
+//     await this.initializeFaissIndex();
+//   }
+
+//   async initializeFaissIndex(): Promise<void> {
+//     try {
+//       const imageClusterEntity: ImageClusterEntity[] =
+//         await this.imageClusterRepository.find();
+
+//       if (imageClusterEntity.length === 0) {
+//         throw new Error('No data found in image_clusters table');
+//       }
+
+//       const embeddings: number[][] = [];
+
+//       imageClusterEntity
+//         .filter(({ clipEmbedding }) => clipEmbedding.length > 0)
+//         .forEach(({ clipEmbedding, imageName, clusterId }) => {
+//           embeddings.push(clipEmbedding);
+//           this.imageData.push({ imageName, clusterId });
+//         });
+
+//       if (embeddings.length === 0) {
+//         throw new Error('No valid embeddings found for FAISS index');
+//       }
+
+//       const dimension = embeddings[0].length;
+//       this.faissIndex = new faiss.IndexFlatL2(dimension);
+
+//       embeddings.forEach((embedding) => this.faissIndex.add(embedding));
+//       this.embeddingMatrix = embeddings;
+
+//       console.log(
+//         `FAISS index initialized with ${this.faissIndex.ntotal()} embeddings.`,
+//       );
+//     } catch (error) {
+//       console.error('Error initializing FAISS index:', error);
+//       throw new Error('Failed to initialize FAISS index');
+//     }
+//   }
+
+//   public async findSimilarProducts(
+//     imageName: string,
+//     topN: number,
+//   ): Promise<IResponse<RecommendationsResDto[]>> {
+//     try {
+//       const targetImage = this.imageData.find(
+//         (img) => img.imageName === imageName,
+//       );
+//       if (!targetImage) {
+//         console.warn(`Image ${imageName} not found in dataset`);
+//         return {
+//           data: [],
+//           message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
+//         };
+//       }
+
+//       const targetIdx = this.imageData.indexOf(targetImage);
+//       const targetVector = this.embeddingMatrix[targetIdx];
+
+//       const results = this.faissIndex.search(targetVector, topN + 1);
+//       const { labels, distances } = results;
+
+//       const similarImages = labels
+//         .map((label, i) => {
+//           if (label !== targetIdx) {
+//             const similarImage = this.imageData[label];
+//             return {
+//               imageName: similarImage.imageName,
+//               similarity: distances[i] * 100,
+//               cluster: similarImage.clusterId,
+//             };
+//           }
+//         })
+//         .filter(Boolean);
+
+//       const items = similarImages.slice(0, topN);
+
+//       if (items.length > 0) {
+//         const result = await Promise.all(
+//           items.map((item) => {
+//             return this.dataService.product.getAllByProperties({
+//               imageName: item.imageName,
+//             });
+//           }),
+//         );
+//         return {
+//           data: result.flat(),
+//           message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
+//         };
+//       }
+//       return {
+//         data: [],
+//         message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
+//       };
+//     } catch (error) {
+//       console.error('Error finding similar images:', error);
+//       throw new Error('Failed to find similar images');
+//     }
+//   }
+// }
+
+// src/use-cases/recommendation/recommendation.usecase.ts
+import { Injectable } from '@nestjs/common';
 import { IDataServices } from 'src/core/abstracts';
 import { RecommendationsResDto } from 'src/core/dto/recommendations/recommendations-res-dto';
 import { IResponse } from 'src/core/interface/response.interface';
+import { MESSAGES } from 'src/infrastructure/common/enum.ts/messages';
+import { FaissService } from 'src/infrastructure/services/faiss/faiss.service';
 
 @Injectable()
-export class RecommendationUsecase implements OnModuleInit {
-  private faissIndex: faiss.IndexFlatL2;
-  private imageData: ImageClusterEntity[] = [];
-  private embeddingMatrix: number[][] = [];
-
+export class RecommendationUsecase {
   constructor(
-    @InjectRepository(ImageClusterModel)
-    private readonly imageClusterRepository: Repository<ImageClusterEntity>,
-    private dataService: IDataServices,
+    private readonly faissService: FaissService,
+    private readonly dataService: IDataServices,
   ) {}
-
-  async onModuleInit() {
-    await this.initializeFaissIndex();
-  }
-
-  async initializeFaissIndex(): Promise<void> {
-    try {
-      const imageClusterEntity: ImageClusterEntity[] =
-        await this.imageClusterRepository.find();
-
-      if (imageClusterEntity.length === 0) {
-        throw new Error('No data found in image_clusters table');
-      }
-
-      const embeddings: number[][] = [];
-
-      imageClusterEntity
-        .filter(({ clipEmbedding }) => clipEmbedding.length > 0)
-        .forEach(({ clipEmbedding, imageName, clusterId }) => {
-          embeddings.push(clipEmbedding);
-          this.imageData.push({ imageName, clusterId });
-        });
-
-      if (embeddings.length === 0) {
-        throw new Error('No valid embeddings found for FAISS index');
-      }
-
-      const dimension = embeddings[0].length;
-      this.faissIndex = new faiss.IndexFlatL2(dimension);
-
-      embeddings.forEach((embedding) => this.faissIndex.add(embedding));
-      this.embeddingMatrix = embeddings;
-
-      console.log(
-        `FAISS index initialized with ${this.faissIndex.ntotal()} embeddings.`,
-      );
-    } catch (error) {
-      console.error('Error initializing FAISS index:', error);
-      throw new Error('Failed to initialize FAISS index');
-    }
-  }
 
   public async findSimilarProducts(
     imageName: string,
     topN: number,
   ): Promise<IResponse<RecommendationsResDto[]>> {
     try {
-      const targetImage = this.imageData.find(
-        (img) => img.imageName === imageName,
-      );
+      const imageData = this.faissService.getImageData();
+      const embeddingMatrix = this.faissService.getEmbeddingMatrix();
+
+      const targetImage = imageData.find((img) => img.imageName === imageName);
       if (!targetImage) {
-        console.warn(`Image ${imageName} not found in dataset`);
-        return {
-          data: [],
-          message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
-        };
+        return { data: [], message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS };
       }
 
-      const targetIdx = this.imageData.indexOf(targetImage);
-      const targetVector = this.embeddingMatrix[targetIdx];
+      const targetIdx = imageData.indexOf(targetImage);
+      const targetVector = embeddingMatrix[targetIdx];
 
-      const results = this.faissIndex.search(targetVector, topN + 1);
+      const results = this.faissService.findSimilarEmbeddings(
+        targetVector,
+        topN + 1,
+      );
       const { labels, distances } = results;
 
       const similarImages = labels
-        .map((label, i) => {
-          if (label !== targetIdx) {
-            const similarImage = this.imageData[label];
-            return {
-              imageName: similarImage.imageName,
-              similarity: distances[i] * 100,
-              cluster: similarImage.clusterId,
-            };
-          }
-        })
-        .filter(Boolean);
+        .filter((label) => label !== targetIdx)
+        .map((label, i) => ({
+          imageName: imageData[label].imageName,
+          similarity: distances[i] * 100,
+          cluster: imageData[label].clusterId,
+        }))
+        .slice(0, topN);
 
-      const items = similarImages.slice(0, topN);
-
-      if (items.length > 0) {
-        const result = await Promise.all(
-          items.map((item) => {
-            return this.dataService.product.getAllByProperties({
-              imageName: item.imageName,
-            });
+      const result = await Promise.all(
+        similarImages.map((item) =>
+          this.dataService.product.getAllByProperties({
+            imageName: item.imageName,
           }),
-        );
-        return {
-          data: result.flat(),
-          message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
-        };
-      }
+        ),
+      );
+
       return {
-        data: [],
+        data: result.flat(),
         message: MESSAGES.RECOMMENDATIONS.GET.SUCCESS,
       };
     } catch (error) {
