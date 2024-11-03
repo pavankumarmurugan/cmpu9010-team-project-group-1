86bb9d21168e2f2a4017347cf8a36e76841357db
Salil Luley <salil.luley@gmail.com>
2024-11-03 20:48:20 +0000

    changes to test build on github

--- smartwardrobe-backend/src/main.ts
+++ smartwardrobe-backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.use(helmet());
   const config = new DocumentBuilder()
     .addBearerAuth()
-    .setTitle('Smart Wardrobe')
+    .setTitle('Smart Wardrobe MS')
     .setDescription('This is Smart Wardrobe ms API description')
     .setVersion('1.0')
     .addTag('Smart Wardrobe MS')
