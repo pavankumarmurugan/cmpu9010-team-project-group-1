7608abdca9c5563eb36865f986f462d3af6aac26
Salil Luley <salil.luley@gmail.com>
2024-11-06 00:41:09 +0000

    Refactored apis to store data as lowercase

--- smartwardrobe-backend/src/core/dto/user/user-req-update-profile.dto.ts
+++ smartwardrobe-backend/src/core/dto/user/user-req-update-profile.dto.ts
@@ -1,20 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateProfileUserReqDTO {
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
+  @Transform(({ value }) => value.toLowerCase())
   readonly firstname: string;
 
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
+  @Transform(({ value }) => value.toLowerCase())
   readonly lastname: string;
 
   @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
+  @Transform(({ value }) => value.toLowerCase())
   readonly email: string;
 
   @ApiProperty({ required: false })
