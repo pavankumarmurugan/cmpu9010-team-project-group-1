3d29d5a663a292d766ea7cef4d2ea6410716b523
Salil Luley <salil.luley@gmail.com>
2024-11-07 19:36:21 +0000

    Added validations to check if user is already in a group

--- smartwardrobe-backend/src/use-cases/group-members/group-members.usecase.ts
+++ smartwardrobe-backend/src/use-cases/group-members/group-members.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { IDataServices } from 'src/core/abstracts';
 import { GroupMemberConvertor } from 'src/core/convertors/group-members/group-members.convertor';
 import { GroupMemberReqDto } from 'src/core/dto/group-members/group.req-dto';
@@ -14,13 +14,29 @@ export class GroupMemberUsecase {
     private convertor: GroupMemberConvertor,
   ) {}
 
-  async create(dto: GroupMemberReqDto): Promise<IResponse<GroupMemberResDto>> {
-    const entity = this.convertor.toEntityFromDto(dto);
-    const createdEntity = await this.dataService.groupMembers.create(entity);
-    return {
-      data: this.convertor.toResDtoFromEntity(createdEntity),
-      message: MESSAGES.GROUP_MEMBERS.CREATE.SUCCESS,
-    };
+  async create(
+    senderId: number,
+    dto: GroupMemberReqDto,
+  ): Promise<IResponse<GroupMemberResDto>> {
+    const { userId, groupId } = dto;
+    if (senderId == userId) {
+      throw new ConflictException(MESSAGES.GROUP_MEMBERS.CREATE.SAME_USER);
+    }
+    const group = await this.dataService.groupMembers.get({
+      userId,
+      groupId,
+    });
+
+    if (!group) {
+      const entity = this.convertor.toEntityFromDto(dto);
+      const createdEntity = await this.dataService.groupMembers.create(entity);
+      return {
+        data: this.convertor.toResDtoFromEntity(createdEntity),
+        message: MESSAGES.GROUP_MEMBERS.CREATE.SUCCESS,
+      };
+    } else {
+      throw new ConflictException(MESSAGES.GROUP_MEMBERS.CREATE.ALREADY);
+    }
   }
 
   async getAll(groupId: number): Promise<IResponse<GroupMemberResDto[]>> {
