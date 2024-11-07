0e6e4a330c478bc9fb0603fafbfce5cc1b7ed1f6
Salil Luley <salil.luley@gmail.com>
2024-11-07 21:18:44 +0000

    Added new response for text search

--- smartwardrobe-backend/src/core/entities/text-ai/text-ai.entity.ts
+++ smartwardrobe-backend/src/core/entities/text-ai/text-ai.entity.ts
@@ -1,14 +1,9 @@
 class SearchTextResults {
   image_name: string;
 }
-class ExpandedQueries {
-  query1: string;
-  query2: string;
-  query3: string;
-}
+
 export class TextResultsEntity {
   constructor() {}
-  readonly expanded_queries?: ExpandedQueries;
   readonly search_results?: SearchTextResults;
 }
 
