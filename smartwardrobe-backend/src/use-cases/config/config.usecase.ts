e6d4c183d555a523fb62538fa3e84224a5d5357d
Salil Luley <salil.luley@gmail.com>
2024-11-12 21:02:06 +0000

    Added code to join notification room

--- smartwardrobe-backend/src/use-cases/config/config.usecase.ts
+++ smartwardrobe-backend/src/use-cases/config/config.usecase.ts
@@ -7,22 +7,22 @@ import { MESSAGES } from 'src/infrastructure/common/enum.ts/messages';
 export class ConfigUsecase {
   constructor(private configService: ConfigService) {}
 
-  async get(): Promise<IResponse<any>> {
-    return {
-      data: {
-        apiKey: this.configService.get('FIREBASE_API_KEY'),
-        authDomain: this.configService.get('FIREBASE_AUTH_DOMAIN'),
-        databaseURL: this.configService.get('FIREBASE_DATABASE_URL'),
-        projectId: this.configService.get('FIREBASE_PROJECT_ID'),
-        storageBucket: this.configService.get('FIREBASE_STORAGE_BUCKET'),
-        messagingSenderId: this.configService.get(
-          'FIREBASE_MESSAGING_SENDER_ID',
-        ),
-        appId: this.configService.get('FIREBASE_APP_ID'),
-        measurementId: this.configService.get('FIREBASE_MEASUREMENT_ID'),
-        vapidKey: this.configService.get('FIREBASE_VAPID_KEY'),
-      },
-      message: MESSAGES.CONFIG.SUCCESS,
-    };
-  }
+  // async get(): Promise<IResponse<any>> {
+  //   return {
+  //     data: {
+  //       apiKey: this.configService.get('FIREBASE_API_KEY'),
+  //       authDomain: this.configService.get('FIREBASE_AUTH_DOMAIN'),
+  //       databaseURL: this.configService.get('FIREBASE_DATABASE_URL'),
+  //       projectId: this.configService.get('FIREBASE_PROJECT_ID'),
+  //       storageBucket: this.configService.get('FIREBASE_STORAGE_BUCKET'),
+  //       messagingSenderId: this.configService.get(
+  //         'FIREBASE_MESSAGING_SENDER_ID',
+  //       ),
+  //       appId: this.configService.get('FIREBASE_APP_ID'),
+  //       measurementId: this.configService.get('FIREBASE_MEASUREMENT_ID'),
+  //       vapidKey: this.configService.get('FIREBASE_VAPID_KEY'),
+  //     },
+  //     message: MESSAGES.CONFIG.SUCCESS,
+  //   };
+  // }
 }
